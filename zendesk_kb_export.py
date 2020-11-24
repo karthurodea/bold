b9c43f5bd4716425fccbd0261e21f25f3f2022a7
Kevin O'Dea <36178352+karthurodea@users.noreply.github.com>
2020-11-23 21:05:35 -0500

    Update zendesk_kb_export.py

--- zendesk_kb_export.py
+++ zendesk_kb_export.py
@@ -2,22 +2,14 @@ import datetime
 import json 
 import requests
 
-credentials = 'kevin.odea2@logmein.com', 'G^^00zbx'
-zendesk = 'https://logmeindev.zendesk.com'
+credentials = <your email>, <your password>
+zendesk = 'https://<your subdomain>.zendesk.com'
 language = 'en-US'
 
 kb_upload = {
     "title": 1,
     "answer": 2
 }
-
-
-# date = datetime.date.today()
-# backup_path = os.path.join(str(date), language)
-# if not os.path.exists(backup_path):
-#     os.makedirs(backup_path)
-
-# log = []
 
 endpoint = zendesk + '/api/v2/help_center/{locale}/articles.json'.format(locale=language.lower())
 while endpoint:
@@ -33,7 +25,7 @@ while endpoint:
         kb_upload["title"] = article['title']
         kb_upload["answer"] = article['body']
         kb_upload_json = json.dumps(kb_upload)
-        url = 'https://kevinddemo.nanorep.co/api/kb/v1/import?account=kevinddemo&kb=English&apiKey=2265560f-14ab-4aad-8be0-66966d9f8b3e&isPreview=false&action=import'
+        url = 'https://kevinddemo.nanorep.co/api/kb/v1/import?account={}&kb={}&apiKey={}&isPreview=false&action=import'
         data = '[' + kb_upload_json + ']'
         headers = {"content-type": "application/json"}
         x = requests.post(url, data=data, headers=headers)
@@ -41,18 +33,3 @@ while endpoint:
         print(response)
 
     endpoint = data['next_page']
-
-# with open(os.path.join(backup_path, '_log.csv'), mode='wt', encoding='utf-8') as f:
-#     writer = csv.writer(f)
-#     writer.writerow( ('File', 'Title', 'Author ID') )
-#     for article in log:
-#         writer.writerow(article)
-
-
-# def _createnewarticle():
-#     url = 'https://kevinddemo.nanorep.co/api/kb/v1/import?account=kevinddemo&kb=English&apiKey=2265560f-14ab-4aad-8be0-66966d9f8b3e&isPreview=false&action=import'
-#     data = '[' + kb_upload_json + ']'
-#     headers = {"content-type": "application/json"}
-#     x = requests.post(url, data=data, headers=headers)
-#     response = x.status_code
-#     print(response)
